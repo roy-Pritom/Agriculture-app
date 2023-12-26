391bf710ce13bff9fa39bf72c0b3db2e1571c315
roy-Pritom <pritombd.hitcse2020@gmail.com>
2023-12-26 11:22:50 +0530

    first commit

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-5xl font-bold my-20 text-blue-500 text-center">Agriculture</h1>
+        </div>
+    );
+};
+
+export default Home;
